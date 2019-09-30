f93c9058c70802a053b42f5dd952834aa19592a9
Costa <marcelo.santos.costa@accenture.com>
2019-09-30 16:36:52 -0300

    configurando banco mysql

--- src/main/java/com/timesheetservice/domain/Employee.java
+++ src/main/java/com/timesheetservice/domain/Employee.java
@@ -1,11 +1,17 @@
 package com.timesheetservice.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
-
+@Entity
 public class Employee implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String role;
